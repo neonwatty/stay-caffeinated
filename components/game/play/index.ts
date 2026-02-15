82be5ffdb4b50261c47908f5831c89557436343f
neonwatty <jermwatt@gmail.com>
2026-02-15 14:11:04 -0700

    feat: add barrel export for play components

---
+++ components/game/play/index.ts
@@ -0,0 +1,7 @@
+export { CharacterSelect } from './CharacterSelect';
+export type { CharacterType } from './CharacterSelect';
+export { GameScene } from './GameScene';
+export { GameHUD } from './GameHUD';
+export { DrinkToolbar } from './DrinkToolbar';
+export { PauseOverlay } from './PauseOverlay';
+export { GameOverOverlay } from './GameOverOverlay';
